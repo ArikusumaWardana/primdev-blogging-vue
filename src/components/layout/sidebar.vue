00a490a820e174774d4cd60d46b3afa698c70365
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-05-25 12:30:10 +0800

    menambah fungsi logout

--- src/components/layout/sidebar.vue
+++ src/components/layout/sidebar.vue
@@ -4,6 +4,11 @@
     {name: 'Dashboard', route: '/dashboard'},
     {name: 'Blog', route: '/dashboard/blogs'},
   ])
+
+  const logout = ()  => {
+    localStorage.removeItem('token')
+    location.reload()
+  }
 </script>
 
 <template>
@@ -12,11 +17,12 @@
       <div class="mt-6 w-full flex flex-col gap-1">
         <div v-for="(item, index) in routeItems" :key="index" class="w-full">
         <router-link :to="item.route" active-class="active">
-          <div class="w-full hover:bg-blue-600 hover:text-white rounded duration-100 px-2 py-2 text-center font-bold">
+          <div class="w-full hover:bg-blue-600 hover:text-white rounded duration-100 px-2 py-2 text-start font-bold uppercase text-sm">
             <p>{{ item.name }}</p>
           </div>
         </router-link>
       </div>
+       <button class="bg-red-600 text-white rounded py-1 font-bold" @click="logout">Log Out</button>
     </div>
   </div>
 </template>
